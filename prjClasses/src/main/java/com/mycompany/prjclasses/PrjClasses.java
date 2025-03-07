fc33578235658fdbd0acb880ff7baa3990cd7c0e
TylerRenison <tylerrenisonwork@gmail.com>
2025-03-07 11:55:20 +0200

    Added a class
    Add a method to display from 2nd class

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,11 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World I just did my first GitHub activity!");
+        
+        newClass nc = new newClass();
+        
+        nc.helloClass();//have to use variable name nc to communicate with the class
+        
+         System.out.println(nc.strSecondClassVariable);// output variable from 2nd class
     }
 }
